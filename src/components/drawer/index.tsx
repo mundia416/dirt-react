caee31c750ebbe7d7e1eea23e79f987ad8f717e6
Mundia <fxmundia416@gmail.com>
2024-10-05 08:21:26 +0200

    bug fix with drawer

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -16,7 +16,7 @@ export default function Drawer({
 }: Props) {
 
     return (
-        <Dialog open={isOpen} onClose={onClose} className="relative z-10">
+        <Dialog open={isOpen} onClose={onClose} className="relative z-50">
             <DialogBackdrop
                 transition
                 className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity duration-500 ease-in-out data-[closed]:opacity-0"

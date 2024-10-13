87d40e4b67d32cbdd8d17e25cfc0f7012d5f6ab4
mundia <fxmundia416@gmail.com>
2024-10-13 16:18:46 +0200

    added classnames to descriptionlist

--- src/components/description-list/index.tsx
+++ src/components/description-list/index.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 export type RowProps = {
     left: React.ReactNode | string,
+    leftClassName?: string
+
     right: React.ReactNode | string
+    rightClassName?: string
 }
 type Props = {
     data: RowProps[]
@@ -18,10 +21,10 @@ export default function DescriptionList({ data }: Props) {
                         key={index}
                         className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
 
-                        {typeof item.left === 'string' ? <dt className="text-sm font-medium leading-6 text-gray-900">{item.left}</dt> : item.left}
+                        {typeof item.left === 'string' ? <dt className={`text-sm font-medium leading-6 text-gray-900 ${item.leftClassName}`}>{item.left}</dt> : item.left}
 
                         <div className='sm:col-span-2'>
-                        {typeof item.right === 'string' ? < dd className="mt-1 text-sm leading-6 text-gray-700  sm:mt-0">{item.right}</dd> : item.right}
+                            {typeof item.right === 'string' ? < dd className={`mt-1 text-sm leading-6 text-gray-700  sm:mt-0 ${item.rightClassName}`}>{item.right}</dd> : item.right}
                         </div>
 
                     </div>
